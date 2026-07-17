0b1353b171ba42646e0b5485361efbdddb2eaeed
Miu-Shioshio <brendagmoro@gmail.com>
2026-07-17 07:17:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+lightGallery(document.getElementById("galeria"),{
+
+    speed:400,
+
+    download:false,
+
+    counter:true,
+
+    zoom:true,
+
+    actualSize:false,
+
+    thumbnail:false,
+
+    selector:"a",
+
+    plugins:[lgZoom],
+
+    mobileSettings:{
+
+        controls:false,
+
+        showCloseIcon:true,
+
+        download:false
+
+    }
+
+});
